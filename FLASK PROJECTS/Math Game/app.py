f88891eec235f9321c503107b6732491fccc6356
Younghoon Cho <younghooncho@Younghoons-MacBook-Pro.local>
2024-04-08 14:24:03 -0400

    flask code

--- FLASK PROJECTS/Math Game/app.py
+++ FLASK PROJECTS/Math Game/app.py
@@ -1,1 +1,19 @@
 # Main entry point
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/send_question')
+def send_question(question):
+    return question
+@app.route('/print')
+def print_anything(statement):
+    return statement
+
+@app.route('/get_answer')
+def get_answer():
+
+    answer_int = request.form.get("answer")
+    
+    return answer_int
